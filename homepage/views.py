22eeb9c1fc66b6a0584be2cc13778a2380529000
Григорий Голоднев <grigoriigolodnev@gmail.com>
2024-03-24 17:23:55 +0300

    added html templates

--- homepage/views.py
+++ homepage/views.py
@@ -4,5 +4,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.template import loader
+
+
 def index(request):
-    return HttpResponse('Главная страница')
+    template_name = 'homepage/index.html'
+    return render(request, template_name)
